e61f50e2475cb9745f8b0dcfe622b1e280a94a7e
moyosorefamojuro <moyosorefamojuro@gmail.com>
2026-04-19 15:51:03 +0100

    Added solution to question 5

---
+++ TechCrush GitHub Assignment/q5.js
@@ -0,0 +1,7 @@
+console.log("                    Question 5: Using 'map' method on arrays");
+console.log("");
+const numbers = [3, 6, 9, 12, 15];
+let numbersDoubled = numbers.map((a) => {
+    return a*2
+})
+console.log(numbersDoubled);
