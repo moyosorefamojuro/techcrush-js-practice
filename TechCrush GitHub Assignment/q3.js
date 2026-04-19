8b081d74a670a0ec379af1a68be37f439e3fa7a3
moyosorefamojuro <moyosorefamojuro@gmail.com>
2026-04-19 15:27:34 +0100

    Added solution to quesion 3

---
+++ TechCrush GitHub Assignment/q3.js
@@ -0,0 +1,8 @@
+console.log("                    Question 3: Function continues");
+console.log("A function called greetUser to output a message to a user");
+console.log("");
+let myName = "Famojuro Moyosore";
+function greetUser(user) {
+    return "Welcome to TechCrush, " + user + "!";
+}
+console.log(greetUser(myName));
