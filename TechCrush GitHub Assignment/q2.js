41b4bed80574f71307a569f5057fb5cd5e65e466
moyosorefamojuro <moyosorefamojuro@gmail.com>
2026-04-19 14:55:45 +0100

    Added solution to question 2

---
+++ TechCrush GitHub Assignment/q2.js
@@ -0,0 +1,7 @@
+console.log("                    Question 2: Arrow Function");
+console.log("          Function to multiply two values using 6 and 7");
+console.log('');
+let multiply = ((a,b) => {
+    return a*b;
+})
+console.log(multiply(6,7));
