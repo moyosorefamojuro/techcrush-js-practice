29ef62ab2cd6fffbd6b09e87a62e2d071ec3812b
moyosorefamojuro <moyosorefamojuro@gmail.com>
2026-04-19 20:45:22 +0100

    Adjusted solution to question 7

--- TechCrush GitHub Assignment/q7.js
+++ TechCrush GitHub Assignment/q7.js
@@ -7,8 +7,12 @@ function formatName(firstName, lastName) {
     trimFirstname = firstName.trim();
     trimLastname = lastName.trim();
     formattedFirstName = trimFirstname.toLowerCase();
+    firstNameedited = formattedFirstName.slice(0,1).toUpperCase() + formattedFirstName.slice(1,8);
     formattedLastName = trimLastname.toUpperCase();
-    fullName = formattedLastName + " " + formattedFirstName;
+    fullName = formattedLastName + ", " + firstNameedited;
     return fullName;
 }
-console.log(formatName(" moyosore        ", "         famojuro "));
+console.log(formatName(" ester        ", "         ogbu "));
+console.log('');
+console.log("Test with my name")
+console.log(formatName("   moyosore  ","             famojuro          "));
