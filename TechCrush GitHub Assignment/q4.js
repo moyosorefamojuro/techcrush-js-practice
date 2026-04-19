813563eb3b1cee20f8b62b149b4f09c5f9bb8a36
moyosorefamojuro <moyosorefamojuro@gmail.com>
2026-04-19 15:42:32 +0100

    Added solution to question 4

---
+++ TechCrush GitHub Assignment/q4.js
@@ -0,0 +1,18 @@
+console.log("                    Question 4: Arrays & Objects");
+console.log("           Storing and working with collections of data");
+console.log("");
+console.log("Given the array below;");
+const fruits = ["mango", "orange", "banana"];
+console.log(fruits);
+console.log("");
+console.log("Adding pineapple to the end");
+let addPin = fruits.push('Pineapple');
+console.log(fruits);
+console.log("");
+console.log("Removing the first element i.e 'mango'");
+let remMango = fruits.shift();
+console.log(fruits);
+console.log("");
+console.log("Logging the updated array and its length");
+console.log(fruits) 
+console.log("Length of array is ", fruits.length);
