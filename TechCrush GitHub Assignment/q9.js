dbfe303223805d43cc590c65f7c58ce6f83d7d16
moyosorefamojuro <moyosorefamojuro@gmail.com>
2026-04-20 08:04:37 +0100

    Added solution to question 9

---
+++ TechCrush GitHub Assignment/q9.js
@@ -0,0 +1,6 @@
+console.log("                    Question 9: Using 'for' loop");
+console.log("                Multiplication table 5 with 'for' loop")
+let table = 5;
+for (let i=1; i<=10; i++) {
+    console.log(table, "*", i, " = ", table * i);
+}
